f1026289f94b39e2e0950f6ac1d83cb3ec887bab
ManeGanesh <39970291+ManeGanesh@users.noreply.github.com>
2023-08-02 01:52:27 +0530

    Add files via upload

---
+++ DSA_Level_1/Recursion and Backtracking/Recursion Backtracking/Target Sum Subsets/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int tar = sc.nextInt();
+        printTargetSumSubsets(arr, 0, "", 0, tar);
+
+    }
+
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int target) {
+
+        if (idx == arr.length) {
+            if (sos == target) {
+                System.out.println(set + ".");
+            }
+
+            return;
+        }
+
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx] + ", ", sos + arr[idx], target);
+        printTargetSumSubsets(arr, idx + 1, set, sos, target);
+
+    }
+
+}
